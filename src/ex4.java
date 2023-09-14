dc28659e0142d1387bb9ea4e90da6469e352b36f
partelsandra <sandra.partel.001@voco.ee>
2023-09-14 21:54:04 +0300

    ex 4 commit

---
+++ src/ex4.java
@@ -0,0 +1,16 @@
+public class ex4 {
+    public static void main(String[] args) {
+       int wholeNumber =9000;
+       double decimalNumber = 0.1;
+       String text = "Zetor";
+
+        System.out.println("Chickens:\n" + wholeNumber);
+        System.out.println("Bacon (kg):\n" + decimalNumber);
+        System.out.println("A tractor:\n" +text);
+
+        // Add an empty line
+        System.out.println();
+
+        System.out.println("In a nutshell:\n" + wholeNumber + "\n" + decimalNumber + "\n" + text);
+    }
+}
