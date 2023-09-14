dad83aefda2040310aff05bd24b69cdb5ac7a12e
partelsandra <sandra.partel.001@voco.ee>
2023-09-15 00:56:37 +0300

    ex25 commit

--- src/week2/ex25.java
+++ src/week2/ex25.java
@@ -1,4 +1,18 @@
 package week2;
 
+import java.util.Scanner;
 public class ex25 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type the first number: ");
+        int number1 = Integer.parseInt(reader.nextLine());
+
+        int sum = 1;
+
+
+        reader.close();
+    }
 }
+
