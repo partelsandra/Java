2b4a075fc2f535de418edafd255eefd3ed3e7d24
partelsandra <sandra.partel.001@voco.ee>
2023-09-14 22:43:22 +0300

    ex 6 commit

---
+++ src/week1/ex6.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class ex6 {
+    public static void main(String[] args) {
+        int x = 73457;
+        int y = 12888;
+        int result = x + y;
+        System.out.println(x + "+" + y + "=" + result);
+    }
+}
