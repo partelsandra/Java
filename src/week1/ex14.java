8034040c368fe0f295c9d659d8b08d723a27443e
partelsandra <sandra.partel.001@voco.ee>
2023-09-14 23:23:01 +0300

    ex 14 commit

---
+++ src/week1/ex14.java
@@ -0,0 +1,22 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ex14 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // program code
+        System.out.println("Type a number: ");
+
+        int number = Integer.parseInt(reader.nextLine());
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+
+        reader.close();
+    }
+}
