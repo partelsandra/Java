006da4ca75a307369fee46fbf1fe0039837c4c01
partelsandra <sandra.partel.001@voco.ee>
2023-09-19 15:47:02 +0300

    ex30 commit

--- src/week2/ex30.java
+++ src/week2/ex30.java
@@ -1,13 +1,18 @@
 package week2;
 
 import java.util.Scanner;
-public class ex27 {
+
+public class ex30 {
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Up to what number?: ");
+        int limit = Integer.parseInt(reader.nextLine());
+
         int number = 1;
 
-        while (number < 101) {
+        while ( number <= limit) {
             System.out.println(number);
             number++;
         }
@@ -15,3 +20,4 @@ public class ex27 {
         reader.close();
     }
 }
+
