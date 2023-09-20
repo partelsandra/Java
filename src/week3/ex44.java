19df470bee7198aa192a23901fd5edbe33b35f12
partelsandra <sandra.partel.001@voco.ee>
2023-09-20 14:13:05 +0300

    ex44 commit

---
+++ src/week3/ex44.java
@@ -0,0 +1,19 @@
+package week3;
+
+import java.util.Scanner;
+
+public class ex44 {
+
+    public static int least(int number1, int number2) {
+        if (number1 < number2) {
+            return number1;
+        } else {
+            return number2;
+        }
+    }
+
+    public static void main(String[] args) {
+        int answer = least(2, 7);
+        System.out.println("Least: " + answer);
+    }
+}
