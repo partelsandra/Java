139db8b6eb77e51b27523efd3992e53a0786077f
partelsandra <sandra.partel.001@voco.ee>
2023-10-31 12:35:09 +0200

    ex commit

--- src/week4/ex75.java
+++ src/week4/ex75.java
@@ -4,9 +4,4 @@ import java.util.Scanner;
 
 public class ex75 {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        scanner.close();
-    }
 }
